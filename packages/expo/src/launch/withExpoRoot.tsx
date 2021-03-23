77d89542e404467a886580f5c2118607c096c007
kodeco835 <81201147+kodeco835@users.noreply.github.com>
2021-03-23 04:43:41 +0000

    Initialize @evanbacon/ncl/add-dev-menu

---
+++ packages/expo/src/launch/withExpoRoot.tsx
@@ -0,0 +1,27 @@
+import * as ErrorRecovery from 'expo-error-recovery';
+import * as React from 'react';
+
+import Notifications from '../Notifications/Notifications';
+import { InitialProps } from './withExpoRoot.types';
+
+export default function withExpoRoot<P extends InitialProps>(
+  AppRootComponent: React.ComponentType<P>
+): React.ComponentType<P> {
+  return function ExpoRoot(props: P) {
+    const didInitialize = React.useRef(false);
+    if (!didInitialize.current) {
+      if (props.exp?.notification) {
+        Notifications._setInitialNotification(props.exp.notification);
+      }
+
+      didInitialize.current = true;
+    }
+
+    const combinedProps = {
+      ...props,
+      exp: { ...props.exp, errorRecovery: ErrorRecovery.recoveredProps },
+    };
+
+    return <AppRootComponent {...combinedProps} />;
+  };
+}
