48297f383b6face1b2fc7727e955f4c93572bc01
kodeco835 <81201147+kodeco835@users.noreply.github.com>
2021-03-23 04:58:10 +0000

    Initialize @evanbacon/notifications/remote-notifications-plugin

---
+++ packages/expo/src/Expo.ts
@@ -0,0 +1,9 @@
+import './Expo.fx';
+
+import * as Logs from './logs/Logs';
+
+export { Logs };
+export { default as registerRootComponent } from './launch/registerRootComponent';
+
+// @ts-ignore
+export { Linking, Notifications } from './deprecated';
