12d2f04368ad553595b51be49ab308386fd5a841
kodeco835 <81201147+kodeco835@users.noreply.github.com>
2021-03-23 04:23:44 +0000

    Initialize @sjchmiela/remove-unimodules-sensors-interface

---
+++ packages/expo-sensors/ios/EXSensors/Modules/SensorModules/EXMagnetometerUncalibrated.m
@@ -0,0 +1,34 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import <EXSensors/EXMagnetometerUncalibrated.h>
+
+@implementation EXMagnetometerUncalibrated
+
+UM_EXPORT_MODULE(ExponentMagnetometerUncalibrated);
+
+- (const NSString *)updateEventName
+{
+  return @"magnetometerUncalibratedDidUpdate";
+}
+
+- (void)setUpdateInterval:(double)updateInterval onSensorService:(id)sensorService
+{
+  [sensorService setMagnetometerUncalibratedUpdateInterval:updateInterval];
+}
+
+- (BOOL)isAvailable:(id)sensorService
+{
+  return [sensorService isMagnetometerUncalibratedAvailable];
+}
+
+- (void)subscribeToSensorService:(id)sensorService withHandler:(void (^)(NSDictionary *event))handlerBlock
+{
+  [sensorService sensorModuleDidSubscribeForMagnetometerUncalibratedUpdates:self withHandler:handlerBlock];
+}
+
+- (void)unsubscribeFromSensorService:(id)sensorService
+{
+  [sensorService sensorModuleDidUnsubscribeForMagnetometerUncalibratedUpdates:self];
+}
+
+@end
