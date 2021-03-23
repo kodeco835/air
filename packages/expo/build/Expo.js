25cc07522b5d02b201f982527bd18e0388462c99
kodeco835 <81201147+kodeco835@users.noreply.github.com>
2021-03-23 04:55:35 +0000

    Initialize @tsapeta/exclude-versioned-code

---
+++ packages/expo/build/Expo.js
@@ -0,0 +1,7 @@
+import './Expo.fx';
+import * as Logs from './logs/Logs';
+export { Logs };
+export { default as registerRootComponent } from './launch/registerRootComponent';
+// @ts-ignore
+export { Linking, Notifications } from './deprecated';
+//# sourceMappingURL=Expo.js.map
