98824d49e63747f8311bd32cce03cd3a4ecc424c
kodeco835 <81201147+kodeco835@users.noreply.github.com>
2021-03-23 04:18:11 +0000

    Initialize @sjchmiela/bare-expo-fix-ncl-assets

---
+++ apps/bare-expo/metro.config.js
@@ -0,0 +1,44 @@
+const { createMetroConfiguration } = require('expo-yarn-workspaces');
+
+const baseConfig = createMetroConfiguration(__dirname);
+
+const assetsLocationsRewrites = {
+  // @react-navigation/stack is hoisted to root node_modules
+  '/node_modules/@react-navigation/stack': '../../node_modules/@react-navigation/stack',
+  // native-component-list is one level up from bare-expo
+  '/native-component-list': '../native-component-list',
+};
+
+module.exports = {
+  ...baseConfig,
+
+  // NOTE(brentvatne): This can be removed when
+  // https://github.com/facebook/metro/issues/290 is fixed.
+  server: {
+    enhanceMiddleware: middleware => {
+      return (req, res, next) => {
+        // When an asset is imported outside the project root, it has wrong path on Android
+        // This happens for the back button in stack, so we fix the path to correct one
+        let assetLocationAlreadyRewritten = false;
+        const originalUrl = req.url;
+        for (const [rewriteFrom, rewriteTo] of Object.entries(assetsLocationsRewrites)) {
+          // While most probably this will never happen, let's guard against it and inform
+          // the developer of any unexpected behavior.
+          if (assetLocationAlreadyRewritten) {
+            console.warn(
+              `Multiple asset location rewrites matched "${originalUrl}". Applying only the first matching rule.`
+            );
+            break;
+          }
+
+          if (req.url.startsWith(rewriteFrom)) {
+            req.url = req.url.replace(rewriteFrom, `/assets/${rewriteTo}`);
+            assetLocationAlreadyRewritten = true;
+          }
+        }
+
+        return middleware(req, res, next);
+      };
+    },
+  },
+};
---
+++ packages/expo/src/launch/withExpoRoot.tsx
@@ -0,0 +1,28 @@
+import * as ErrorRecovery from 'expo-error-recovery';
+import * as React from 'react';
+
+import Notifications from '../Notifications/Notifications';
+import { InitialProps } from './withExpoRoot.types';
+
+export default function withExpoRoot<P extends InitialProps>(
+  AppRootComponent: React.ComponentType<P>
+): React.ComponentType<P> {
+  return function ExpoRoot(props: P) {
+    const didInitialize = React.useRef(false);
+    if (!didInitialize.current) {
+      const { exp } = props;
+      if (exp.notification) {
+        Notifications._setInitialNotification(exp.notification);
+      }
+
+      didInitialize.current = true;
+    }
+
+    const combinedProps = {
+      ...props,
+      exp: { ...props.exp, errorRecovery: ErrorRecovery.recoveredProps },
+    };
+
+    return <AppRootComponent {...combinedProps} />;
+  };
+}
